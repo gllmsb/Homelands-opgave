a3c46e43e4ab1e6ff901ae16daeb99a8dbfb1912
gllmsb <val29s03@gmail.com>
2025-02-24 10:40:25 +0100

    Routes

--- homelands/src/App.jsx
+++ homelands/src/App.jsx
@@ -1,9 +1,23 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { mainLayout } from './layout/mainLayout'
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
+import { Sale } from './pages/Sale'
+
 
 function App() {
 
   return (
     <>
+      <Router>
+        <Routes>
+          <Route element={<mainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="sale" element={<Sale />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
